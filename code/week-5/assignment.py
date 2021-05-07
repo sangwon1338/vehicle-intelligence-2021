1a5bb90e31d0aecd7f18a6f1624c369f04f73e80
sangown1338 <swbaek1338@gmail.com>
2021-05-07 03:45:20 +0000

    update

--- code/week-5/assignment.py
+++ code/week-5/assignment.py
@@ -67,18 +67,79 @@ def optimum_policy_2D(grid, init, goal, cost):
         # Compute the value function for each state and
         # update policy function accordingly.
         for y, x, t in p:
+            #print(y, ' ', x, ' ', t)
             # Mark the final state with a special value that we will
             # use in generating the final path policy.
             if (y, x) == goal and value[(t, y, x)] > 0:
                 # TODO: implement code.
-                pass
+                # M2021077 Sunpil Kim
+                value[(t,y,x)] = 0
+                # Final Marker --> -444
+                policy[(t,y,x)] = -444
+                change = True
             # Try to use simple arithmetic to capture state transitions.
             elif grid[(y, x)] == 0:
-                # TODO: implement code.
-                pass
+
+                for f_idx in range(len(forward)):
+                    # get post position
+                    x_tmp = x + forward[f_idx][1]
+                    y_tmp = y + forward[f_idx][0]
+
+                    # boundary check
+                    # Be Careful -> len_x < len(grid[0])    --> 1 hour source bug ㅡㅡ
+                    if x_tmp >= 0 and x_tmp < len(grid[0]) and y_tmp >= 0 and y_tmp < len(grid) and grid[y_tmp][x_tmp] == 0:
+                        post_tmp = value[(f_idx, y_tmp, x_tmp)]
+                        #print(post_tmp)
+
+                        for act_idx in range(len(action)):
+                            if (t + action[act_idx]) % len(forward) == f_idx:
+                                v_tmp = post_tmp + cost[act_idx]
+
+                                # cost coparison & change
+                                if v_tmp < value[(t,y,x)]:
+                                    value[(t,y,x)] = v_tmp
+                                    policy[(t,y,x)] = action[act_idx]
+                                    change = True
+
     # Now navigate through the policy table to generate a
     # sequence of actions to take to follow the optimal path.
-    # TODO: implement code.
+   
+
+  
+
+    # init position & orientation
+    y = init[0]
+    x = init[1]
+    f = init[2]
+
+    if policy[(f,y,x)] == -1:
+        policy2D[(y,x)] = action_name[0]
+    elif policy[(f,y,x)] == 0:
+        policy2D[(y,x)] = action_name[1]
+    elif policy[(f,y,x)] == 1:
+        policy2D[(y,x)] = action_name[2]
+    else:
+        policy2D[(y,x)] = "*"
+
+    # visualization
+    while policy[(f,y,x)] != -444:
+        if policy[(f,y,x)] == -1:
+            f = (f - 1)%4
+        elif policy[(f,y,x)] == 1:
+            f = (f + 1)%4
+
+        x += forward[f][1]
+        y += forward[f][0]
+
+        if policy[(f,y,x)] == -1:
+            policy2D[(y,x)] = action_name[0]
+        elif policy[(f,y,x)] == 0:
+            policy2D[(y,x)] = action_name[1]
+        elif policy[(f,y,x)] == 1:
+            policy2D[(y,x)] = action_name[2]
+        else:
+            # final state is visualized as "*" star marker
+            policy2D[(y,x)] = "*"
 
     # Return the optimum policy generated above.
     return policy2D
